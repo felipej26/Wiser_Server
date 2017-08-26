4ed231c07530decff3c17bd2f1835ff564317b60
Jefferson Felipe <felipej2626@gmail.com>
2017-08-25 21:56:54 -0300

    Setado o adapter correto

--- config/models.js
+++ config/models.js
@@ -28,7 +28,7 @@ module.exports.models = {
   *                                                                          *
   ***************************************************************************/
 
-  connection: 'mySQLAdapter',
+  connection: 'mySqlProd',
   migrate: 'alter'
 
 };
